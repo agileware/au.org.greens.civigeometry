dccf0b497a67f739b9ae5960aedef57d1636bb20
John Twyman <john.twyman@greens.org.au>
2020-09-17 17:00:25 +1000

    Fix typo for failing unit test

--- api/v3/Geometry.php
+++ api/v3/Geometry.php
@@ -158,14 +158,14 @@ function civicrm_api3_geometry_get($params) {
 }
 
 /**
- * Geomety.getCollections
+ * Geometry.getCollections
  *
  * @param array $params
  * @return array API result descriptor
  * @throws API_Exception
  */
 function civicrm_api3_geometry_getcollection($params) {
-  return _civicrm_api3_basic_get('CRM_CiviGeometry_BAO_GeometryCollectionGeometry', $params);
+  return _civicrm_api3_basic_get('CRM_CiviGeometry_DAO_GeometryCollectionGeometry', $params);
 }
 
 /**
